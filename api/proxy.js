712878ef5ecc37335b47f87f4bbea20540a04bc8
gelthemu <gelthemu@gmail.com>
2024-07-06 22:57:21 +0300

    CORS headers

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,19 @@
-const httpProxy = require('http-proxy');
-const url = require('url');
+const httpProxy = require("http-proxy");
+const cors = require("cors")();
 
 const proxy = httpProxy.createProxyServer();
 
 export default function handler(req, res) {
-  const targetUrl = 'https://fmradiohub.in/play?url=http://mp.techsysug.com:21563/;stream';
-  const queryString = url.parse(req.url).query;
+  cors(req, res, () => {
+    const targetUrl =
+      "https://fmradiohub.in/play?url=http://mp.techsysug.com:21563/;stream";
 
-  proxy.web(req, res, { target: `${targetUrl}&${queryString}`, changeOrigin: true }, (error) => {
-    res.status(500).send('Proxy error');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    proxy.web(req, res, { target: targetUrl, changeOrigin: true }, (error) => {
+      res.status(500).send("Proxy error");
+    });
   });
 }
